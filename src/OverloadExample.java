b9aebdfc5427f51ea7b2d0b7403c99a09773086c
Hitohito1023 <ryotakinase1023@gmail.com>
2021-04-07 13:27:42 +0900

    [finish]Chapter6

--- src/OverloadExample.java
+++ src/OverloadExample.java
@@ -2,6 +2,24 @@ class PointB {
 	int x;
 	int y;
 	
+	PointB() {
+		System.out.println("引数のないコンストラクタが実行されました");
+		this.x = 0;
+		this.y = 0;
+	}
+	
+	PointB(PointB p) {
+		System.out.println("引数が１つのコンストラクタが実行されました");
+		this.x = p.x;
+		this.y = p.y;
+	}
+	
+	PointB(int x, int y) {
+		System.out.println("引数が２つのコンストラクタが実行されました");
+		this.x = x;
+		this.y = y;
+	}
+	
 	void set(int x, int y) {
 		this.x = x;
 		this.y = y;
@@ -18,12 +36,15 @@ public class OverloadExample {
 		PointB p1 = new PointB();
 		System.out.println("p1の座標は（" + p1.x + ", " + p1.y + "）");
 		
-		p1.set(10, 0);
-		System.out.println("p1の座標は（" + p1.x + ", " + p1.y + "）");
+//		p1.set(10, 0);
+//		System.out.println("p1の座標は（" + p1.x + ", " + p1.y + "）");
 		
-		PointB p2 = new PointB();
-		p2.set(p1);
+		PointB p2 = new PointB(10, 20);
+//		p2.set(p1);
 		System.out.println("p2の座標は（" + p2.x + ", " + p2.y + "）");
+		
+		PointB p3 = new PointB(p2);
+		System.out.println("p3の座標は（" + p3.x + ", " + p3.y + "）");
 
 	}
 
