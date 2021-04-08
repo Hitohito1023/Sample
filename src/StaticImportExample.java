9c93cc2139be6ef55bf7f439e9aef6604c0a98c2
Hitohito1023 <ryotakinase1023@gmail.com>
2021-04-08 18:32:32 +0900

    [finish]Chapter13

---
+++ src/StaticImportExample.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.*;
+
+public class StaticImportExample {
+	public static void main(String[] args) {
+		System.out.println("PI=" + PI);
+		System.out.println("abs(-2)=" + abs(-2));
+	}
+
+}
